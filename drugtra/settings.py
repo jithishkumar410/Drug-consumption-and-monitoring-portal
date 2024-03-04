0868d6f5ad79c29d153a3c1d9feedb15a88e2802
jithish kumar <97963977+jithishkumar410@users.noreply.github.com>
2024-03-04 11:55:57 +0530

    Update settings.py

--- drugtra/settings.py
+++ drugtra/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qmbk5ha5gewk-kh308mc8lr5)f8ac_(n3$gn$al=1(##dnpw-c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
